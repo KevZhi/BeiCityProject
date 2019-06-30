45b825e40acddd43fa3572e9fff5f99f5bb7d726
confeather <i@confeather.com>
2019-07-01 00:03:28 +0800

    更新剧本编辑器

--- Assets/Editor/TextEditor.cs
+++ Assets/Editor/TextEditor.cs
@@ -16,10 +16,12 @@ public class TextEditor : EditorWindow
     bool isEdit = false;
     bool isTrue = false;
     private static string scriptPath = "Resources/Text/";
+
     [MenuItem("Window/Text Editor")]
     static void Init()
     {
-        GetWindow(typeof(TextEditor));
+       var window =  GetWindow(typeof(TextEditor));
+       
     }
 
     void OnGUI()
@@ -76,10 +78,10 @@ public class TextEditor : EditorWindow
 
     private void ShowEditPanel()
     {
+        
         var options = new[] { GUILayout.Width(200), GUILayout.Height(30) };
-
         //ShowNotification(new GUIContent("EditText"));
-        editName = EditorGUILayout.TextArea(editName);
+        EditorGUILayout.LabelField("Text Name : " + editName);
         EditorGUILayout.Space();
         if (GUILayout.Button("Open"))
         {
@@ -87,7 +89,8 @@ public class TextEditor : EditorWindow
             string path = EditorUtility.OpenFilePanel("Open text", Application.dataPath + scriptPath, "txt");
             if (path != "")
             {
-                //EditorGUILayout.LabelField(path.Substring(0, path.LastIndexOf('/')), options);
+                dataTypesIndexs = new List<int>();
+                dataTypes = new List<string>();
 
                 editName = path.Replace(path.Substring(0, path.LastIndexOf('/') + 1), "");
                 editName = editName.Substring(0, editName.LastIndexOf('.'));
@@ -95,25 +98,45 @@ public class TextEditor : EditorWindow
 
                 //scriptContent = DoCreateText.Read(scriptPath + editName + ".txt");
                 LoadScript(editName);
+                //path = "";
             }
 
         }
+
         EditorGUILayout.Space();
         //scriptContent = EditorGUILayout.TextArea(scriptContent);
-        for (int i = 0; i < txt.Count; i++)
+
+        if (dataTypesIndexs.Count< txt.Count)
         {
-            //LoadSentence(i);
+            for (int i = 0; i < txt.Count; i++)
+            {
+                //LoadSentence(i);
+                
+                HandleData(LoadSentence(i));
+                //txt[i] = EditorGUILayout.TextArea(txt[i]);
+                //if (EditorGUILayout.TextArea(txt[i]) == txt[i])
+                //{
+                //    break;
+                //}
+            }
+        }
+        
+        for (int i = 0; i < dataTypesIndexs.Count; i++)
+        {
             EditorGUILayout.BeginVertical("box");
-            HandleData(LoadSentence(i));
-            txt[i] = EditorGUILayout.TextArea(txt[i]);
-            EditorGUILayout.EndVertical();
+            EditorGUILayout.LabelField(dataTypes[i]);
             EditorGUILayout.Space();
+            dataTypesIndexs[i] = EditorGUILayout.Popup(dataTypesIndexs[i], types);
+            EditorGUILayout.Space();
+            EditorGUILayout.EndVertical();
         }
 
-
+        Debug.Log("data types index count " + dataTypesIndexs.Count);
+        Debug.Log("data types " + dataTypes.Count);
     }
 
     private static List<string> txt = new List<string>();
+   
     private void LoadScript(string txtFileName)
     {
         txt = new List<string>();
@@ -124,6 +147,21 @@ public class TextEditor : EditorWindow
 
         }
         stream.Close();
+
+        //for (int i = 0; i < txt.Count; i++)
+        //{
+        //    //LoadSentence(i);
+        //    EditorGUILayout.BeginVertical("box");
+        //    HandleData(LoadSentence(i),i);
+        //    //txt[i] = EditorGUILayout.TextArea(txt[i]);
+        //    //if (EditorGUILayout.TextArea(txt[i]) == txt[i])
+        //    //{
+        //    //    break;
+        //    //}
+
+        //    EditorGUILayout.EndVertical();
+        //    EditorGUILayout.Space();
+        //}
     }
 
     /// <summary>
@@ -161,20 +199,37 @@ public class TextEditor : EditorWindow
             return null;
         }
     }
-
-    public void HandleData(ScriptData data)
+    List<int> dataTypesIndexs = new List<int>();
+    List<string> dataTypes = new List<string>();
+    string[] types = new string[] {
+            ScriptType.Dialogue.ToString(),
+            ScriptType.FadeInRole.ToString(),
+            ScriptType.FadeOutRole.ToString()
+        };
+    private void HandleData(ScriptData data)
     {
+        //indexs = new List<int>();
         string type = "";
         if (data == null)
             return;
 
+        //EditorGUILayout.LabelField(ScriptType.Dialogue.ToString());
+        //gUIContent.
         switch (data.type)
         {
             case ScriptType.Dialogue:
+                dataTypesIndexs.Add(0);
+                dataTypes.Add(ScriptType.Dialogue.ToString());
+                //EditorGUILayout.DropdownButton(new GUIContent("a" + "b"), FocusType.Keyboard, options);
+                //index = EditorGUILayout.Popup(index, types);
                 type = "Dialogue";
                 type = EditorGUILayout.TextArea(type);
                 break;
             case ScriptType.FadeInRole:
+                dataTypesIndexs.Add(1);
+                dataTypes.Add(ScriptType.FadeInRole.ToString());
+                //EditorGUILayout.DropdownButton(new GUIContent("a" + "b"), FocusType.Keyboard, options);
+                //index = EditorGUILayout.Popup(index, types);
                 type = "FadeInRole";
                 type = EditorGUILayout.TextArea(type);
                 //gm.keyboardInput.action = true;
@@ -196,6 +251,8 @@ public class TextEditor : EditorWindow
                 //}
                 break;
             case ScriptType.FadeOutRole:
+                dataTypesIndexs.Add(2);
+                dataTypes.Add(ScriptType.FadeOutRole.ToString());
                 type = "FadeOutRole";
                 type = EditorGUILayout.TextArea(type);
                 //switch (data.pos)
@@ -218,5 +275,4 @@ public class TextEditor : EditorWindow
                 break;
         }
     }
-
 }
